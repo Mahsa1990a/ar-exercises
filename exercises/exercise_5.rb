921825c46015f2201c3fa643545c88274d254c28
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-01 20:53:39 +0000

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total = Store.sum(:annual_revenue)  # syntax: sum(:column_name)
+
+@average = Store.average(:annual_revenue)   # syntax: average(:column_name)
+
+@million = Store.where("annual_revenue >= 1000000").count()
+
+puts @total
+puts @average
+puts @million
+
+puts "Exercise 5 DONE"
