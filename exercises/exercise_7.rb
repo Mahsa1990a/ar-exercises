b0962047e28fac9b60a2670d46a98c6ef0b91e8f
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-01 23:17:30 +0000

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name (store it in a variable)
+puts "Store name: "
+@new_store = gets.chomp
+
+# Attempt to create a store with the inputted name
+newStore = Store.create(name: @new_store)
+
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line)
+# after you attempt to save/create the record
+puts "Error: #{newStore.errors.full_messages}"
+
+
+puts "Exercise 7 DONE"
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,10 @@
 class Employee < ActiveRecord::Base
 
   belongs_to :store
-  
+  validates :first_name, presence: true  # Employees must always have a first name present
+  validates :last_name, presence: true   # Employees must always have a last name present
+  validates :hourly_rate, inclusion: {in: 40..200}  # Employees have a hourly_rate that is a number (integer) between 40 and 200
+  # Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+  validates :store_id, presence: true
+
 end
